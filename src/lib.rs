583b2fac750905918814390f66e1f5629f21b610
Nisrina Alya Nabilah <nisrina.alya@ui.ac.id>
2026-04-21 13:40:36 +0700

    (Bonus) Function improvement

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,25 @@ impl ThreadPool {
         }
     }
 
+    pub fn build(size: usize) -> Result<ThreadPool, String> {
+        if size == 0 {
+            return Err(String::from("ThreadPool size must be greater than 0"));
+        }
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        Ok(ThreadPool {
+            workers,
+            sender: Some(sender),
+        })
+    }
+
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
